9033c5ab2fd52a5f1f345992d55fafa730dc3840
zanton173 <zanton173@gmail.com>
2023-09-14 10:34:49 -0400

    Created index.html with boilerplate for project

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	"text/template"
 )
 
 func main() {
 
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("./index.html"))
+		tmpl.Execute(w, nil)
+	}
+
+	http.HandleFunc("/read-my-joke", h1)
 	log.Fatal(http.ListenAndServe(":8040", nil))
 }
